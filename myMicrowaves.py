bf9ccf02ac9c88a5baf16d5403cd44c9977a0f12
Mario Sotelo <mgsotelo@pucp.pe>
2018-07-15 05:18:25 -0500

    Development (#2)
    * Added gui
    
    Changed gitignore
    Generated gui
    Using Qt5
    
    * Organized code for gui
    
    Organized code for gui
    
    * Added live views on gui :+1:
    
    Finally it loads data from Arduino
    Added myMicrowaves support
    Completed MyApp
    Modified GUI
    
    * Added audio capabilities
    
    Input will be collected using 3.5mm audio jack input (as audio) because we are going to use a frequency range from 1KHz to 20KHz (or something like that)
    Found a bug where input does not start until we change plot options
    
    * Fixed audio input bug
    
    YRange was causing some troubles
    
    * Updated myMicrowaves
    
    Added threads for better performance
    Calculating distance through in-code parameters (TO DO some GUI to change or modify them)
    
    * Making some changes (TODOs anyway)

--- myMicrowaves.py
+++ myMicrowaves.py
@@ -31,14 +31,14 @@ class AudioThread(QtCore.QThread):
     def __init__(self, *args, **kwargs):
         QtCore.QThread.__init__(self, *args, **kwargs)
         self.fs = 44100
-        self.stream = sd.InputStream(samplerate=self.fs, channels=1, device=0)
+        self.stream = sd.InputStream(samplerate=self.fs, channels=2, device=0)
         # print(self.stream)
 
     def run(self):
         with self.stream:
             while True:
                 data, overflow = self.stream.read(1)
-                if data:
+                if data.any():
                     datos = data[0]
                     self.dataChanged.emit(np.float(datos[0]))
         
@@ -46,54 +46,105 @@ class AudioThread(QtCore.QThread):
 class FMCWoperations:
     def __init__(self):
         self.contador = 0
-        self.contadortime = 0
+        self.contador2 = 0
+        self.contador3 = 0
+        # contadortime = 0
         # cambiar a arduino si se quiere usar un arduino
-        self.thread = AudioThread()
+        self.thread1 = AudioThread()
+        self.thread2 = AudioThread()
+        self.thread3 = AudioThread()
         # self.thread = ArduinoThread()
-        self.thread.start()
-    
+        self.numMuestras = 882
+        self.y1 = np.zeros(self.numMuestras, dtype=float)
+        self.xd = np.zeros(1)
+
     def frequencyPlot(self):
-        
-        self.thread.dataChanged.connect(self.updateFrequencyPlot)
+        self.thread1.start()
+        self.thread1.dataChanged.connect(self.updateFrequencyPlot)
         self.win = pg.GraphicsWindow()
         self.win.setWindowTitle('Frecuencia(LIVE) - TEL236')
-        # print(sd.query_devices())
         self.p1 = self.win.addPlot()
-        # self.p1.setYRange(0, 1024, padding=0)
+        self.p1.setYRange(0, 1e-5, padding=0)
         self.curva1 = self.p1.plot()
-        self.y1 = np.zeros(1024, dtype=float)
-        print(self.win.setAntialiasing(True))
+        self.p_max1 = self.win.addPlot()
+        self.p_max1.setYRange(0, 1e-5, padding=0)
+        self.puntito1 = self.p_max1.plot()
+        # print(self.win.setAntialiasing(True))
 
     def updateFrequencyPlot(self, data):
-
-        fs = self.thread.fs
-        if self.contador > 1023:
+        fs = self.thread1.fs
+        if self.contador > self.numMuestras - 1:
             self.contador = 0
-            self.y1 = np.zeros(1024, dtype=float)
-        elif self.contador == 1023:
-            f, Pwelch_spec = signal.welch(self.y1, fs, scaling='spectrum')
-            self.curva1.setData(f, Pwelch_spec, pen='r')
+            self.y1 = np.zeros(self.numMuestras, dtype=float)
+        elif self.contador == self.numMuestras - 1:
+            data = signal.welch(self.y1, fs, scaling='spectrum')
+            f, Pwelch_spec = data
+            self.max_index = np.argmax(Pwelch_spec)
+            # max_amp = Pwelch_spec[max_index]
+            self.Puntito_spec = np.zeros(len(data[0]), dtype=float)
+            # Puntito_spec[max_index] = max_amp
+            # Maxima amplitud para que solo varía dependiendo de la frecuencia
+            self.Puntito_spec[self.max_index] = 6e-6
+            self.puntito1.setData(f, self.Puntito_spec, pen=None, symbol='o')
+            self.curva1.setData(f, Pwelch_spec, pen='y')
             self.contador += 1
         else:
             self.y1[self.contador] = data
             self.contador += 1
         
     def frequencyDist(self):
-        pass
+        self.thread2.start()
+        self.thread2.dataChanged.connect(self.updateFrequencyDist)
+        self.win3 = pg.GraphicsWindow()
+        self.win3.setWindowTitle('Distancia (LIVE) - TEL236')
+        self.p3 = self.win3.addPlot()
+        self.p_dis = self.p3.plot(pen=None, symbol='o')
+        self.p3.setYRange(0, 15, padding=0)
+        self.p3.showGrid(True, True, 0.7)
+        self.R1 = 0  # la distancia que queremos hallar
+        
+        #################################################################
+        # parametros para modificar :D
+        self.BW = 490e6  # ancho de banda
+        self.T = self.numMuestras / self.thread2.fs  # periodo de muestreo
+        self.Vg = 3e8  # velocidad de la luz aprox
+        #################################################################
 
-    def timePlot(self):
-        self.thread.dataChanged.connect(self.updateTimePlot)
+    def updateFrequencyDist(self, data):
+        fs = self.thread2.fs
+        if self.contador2 > self.numMuestras - 1:
+            self.contador2 = 0
+            self.y1 = np.zeros(self.numMuestras, dtype=float)
+        elif self.contador2 == self.numMuestras - 1:
+            f, Pwelch_spec = signal.welch(self.y1, fs, scaling='spectrum')
+            self.max_index = np.argmax(Pwelch_spec)
+            # print(self.T + " " + self.Vg)
+            # print(f[self.max_index])
+            self.R1 = (self.T * self.Vg * f[self.max_index]) / (2 * self.BW)
+            # print("frecuencia = ", f[self.max_index], " distancia = "
+            # , self.R1, " amplitud = ", Pwelch_spec[self.max_index])
+            self.xd[0] = self.R1
+            # print(self.xd)
+            self.p_dis.setData(self.xd)
+            self.contador2 += 1
+        else:
+            self.y1[self.contador2] = data
+            self.contador2 += 1
+        
+    def timePlot(self):        
+        self.thread3.start()
+        self.thread3.dataChanged.connect(self.updateTimePlot)
         self.win2 = pg.GraphicsWindow()
         self.win2.setWindowTitle('Tiempo(LIVE) - TEL236')
         self.p2 = self.win2.addPlot()
         self.curva2 = self.p2.plot()
-        self.y2 = np.zeros(1024, dtype=float)
-
+        self.y2 = np.zeros(self.numMuestras, dtype=float)
+        
     def updateTimePlot(self, data):
-        if self.contadortime > 1023:
-            self.y2 = np.zeros(1024, dtype=float)
-            self.contadortime = 0
+        if self.contador3 > self.numMuestras - 1:
+            self.y2 = np.zeros(self.numMuestras, dtype=float)
+            self.contador3 = 0
+            # self.curva2.setData(self.y2)
         else:
-            self.y2[self.contadortime] = data
-            self.contadortime += 1
-        self.curva2.setData(self.y2)
+            self.y2[self.contador3] = data
+            self.contador3 += 1
